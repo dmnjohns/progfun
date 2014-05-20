0b50e5e3b15ad33f03e03caa8239f827a92b566d
Damien Johnson <damien.johnson@rackspace.com>
2014-05-20 10:05:11 -0500

    Google and Apple sets

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -212,8 +212,8 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet = TweetReader.allTweets.filter((t: Tweet) => google.exists((s: String) => t.text.contains(s)))
+  lazy val appleTweets: TweetSet = TweetReader.allTweets.filter((t: Tweet) => apple.exists((s: String) => t.text.contains(s)))
 
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
